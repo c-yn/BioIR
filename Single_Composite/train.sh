734f3d5a1831c5d30ad4f87a238244d1c4651dae
Yuning Cui <94985227+c-yn@users.noreply.github.com>
2026-01-06 23:40:10 +0800

    Update train.sh

--- Single_Composite/train.sh
+++ Single_Composite/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CONFIG=$1
 
-CUDA_VISIBLE_DEVICES=2 torchrun --nproc_per_node=1 --master_port=4322 basicsr/train.py -opt $CONFIG --launcher pytorch
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=4322 basicsr/train.py -opt $CONFIG --launcher pytorch
 
 
 
